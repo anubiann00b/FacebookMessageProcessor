82968f5b39a0c9eade73249664f982418f5fecb6
Shreyas <skraman1999@gmail.com>
2016-06-23 15:11:05 -0700

    Include all threads

--- src/main/scala/me/shreyasr/fbmp/Parser.scala
+++ src/main/scala/me/shreyasr/fbmp/Parser.scala
@@ -1,5 +1,6 @@
 package me.shreyasr.fbmp
 
+import java.io._
 import java.text.{DateFormat, DateFormatSymbols, SimpleDateFormat}
 import java.util.{Date, Locale}
 
@@ -8,48 +9,63 @@ import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.model.Document
 
+
 object Parser {
 
   def main(args: Array[String]) {
     val filename = "messages.htm"
-    val person = "Jawahar Madan"
+    val me = "My Name"
+    val person = "Other Name"
 
-    getMessages(JsoupBrowser().parseFile(filename), person, {
+    val readableFile = new PrintWriter(new File(s"$person.txt"))
+    val rawFile = new PrintWriter(new File(s"$person-raw.txt"))
+
+    getMessages(JsoupBrowser().parseFile(filename), me, person, {
       val df = new SimpleDateFormat("EEEE, MMMM dd, yyyy 'at' k:mma z", Locale.US)
       df.setDateFormatSymbols({
         val symbols = new DateFormatSymbols(Locale.getDefault)
         symbols.setAmPmStrings(Array[String]("am", "pm")); symbols
       }); df
-    }).foreach(println)
+    }).foreach(m => {
+      readableFile.write(m.toString + "\n")
+      rawFile.write(m.message.toLowerCase.filterNot(".?!<>".toSet) + "\n")
+    })
+
+    readableFile.close()
+    rawFile.close()
+    // cat {NAME}-raw.txt | tr [:space:] '\n' | grep -v "^\s*$" | sort | uniq -c | sort -bnr >> {NAME}-wordlist.txt
   }
 
-  def getMessages(document: Document, person: String, df: DateFormat): List[Message] = {
-    (document >> elementList(".thread")).find(thread => {
-      val text = thread.innerHtml
-      val p1 = text.slice(0, text.indexOf(","))
-      val p2 = text.slice(text.indexOf(",") + 2, text.indexOf("\n"))
-      p1 == person || p2 == person
-    }) match {
-      case None => println("Person not found"); List()
-      case Some(thread) => thread.flatMap(thread => {
+  def getMessages(document: Document, me: String, person: String, df: DateFormat): List[Message] = {
+    val p = { var i = 0; () => { i += 1; i} }
+    (document >> elementList(".thread"))
+      .map(t => (t, t.innerHtml.slice(0, t.innerHtml.indexOf('\n')).split(", ")))
+      .filter { case (_, names) => names.size == 2 && names.contains(me) && names.contains(person) }
+      .map { case (thread, names) => thread }
+      .map(thread => {
         val dates = (thread >> elementList(".message")).map(m => (m >> element(".meta")).text).map(df.parse)
         val senders = (thread >> elementList(".message")).map(m => (m >> element(".user")).text)
         val messages = (thread >> elementList("p")).map(_.innerHtml)
         zip3(dates, senders, messages)
-      }).map {
+      }).filter(_.forall({
+        case (date, sender, message) => sender == me || sender == person
+      })).flatten.map {
         case (date, sender, message) => new Message(date, sender, message)
-      }.toList.reverse
-    }
+      }.reverse.sorted
   }
 
   def zip3[A, B, C](l1 : List[A], l2 : List[B],l3 : List[C]): List[(A, B, C)] =
     l1.zip(l2).zip(l3).map { case ((a, b), c) => (a, b, c) }
 
-  private class Message(val date: Date, val sender: String, val message: String) {
+  class Message(val date: Date, val sender: String, val message: String) extends Ordered[Message] {
+
+    override def compare(that: Message): Int = this.date.compareTo(that.date)
+
     override def toString: String =
       s"${Message.renderDateFormat.format(date)} ${sender.substring(0, sender.indexOf(" "))}: $message"
   }
-  private object Message {
+
+  object Message {
     val renderDateFormat: DateFormat = new SimpleDateFormat("MM/dd/yy hh:mm")
   }
 }
